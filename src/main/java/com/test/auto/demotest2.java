6b98ab5a7f7b619a49ad1a13f7846fc124289a46
keyshah <keyurshah12345@gmail.com>
2021-02-12 12:43:20 +0530

    commit

--- src/main/java/com/test/auto/demotest2.java
+++ src/main/java/com/test/auto/demotest2.java
@@ -4,7 +4,7 @@ public class demotest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test2");
+		System.out.println("Test2-update");
 	}
 
 }
